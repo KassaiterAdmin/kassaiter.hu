69ad6ef9b088ffaf4241bcdf874e58bd29d89a16
KassaiterAdmin <info@z-poc.hu>
2018-01-10 10:52:54 +0100

    add info to jaki kapolna page

--- resources/views/kassaiter/jaki.blade.php
+++ resources/views/kassaiter/jaki.blade.php
@@ -14,3 +14,30 @@ A tartós, nemes anyagok felhasználásával befejeződött nagyszabású épít
 <br><br>
 Alpár Ignác koncepciója az volt, hogy a pályázati kiírásnak megfelelően a történelmi és művészi értékű hazai emlékek egészét-részleteit örökítse meg, harmonikus egésszé ötvözze.
 Törekvése sikerrel járt, elismerést szerezve a hazai és külföldi szakértők, nemkülönben a lakosság, turisták körében.
+<br><br>
+<strong>Leírása:</strong>
+<br><br>
+A Történelmi Főcsoport első része a korábban Drót- majd Széchenyi sziget északnyugati sarkán épített román-épületegyüttes.
+Annak legjellegzetesebb részlete a Vas-megyei Jáki templom kapuzatának hű mása.
+Az 1256-ban, Omodé püspök által felszentelt, és Alpárnak mintául szolgáló templom egyéb részleteit nem tanulmányozhatjuk a Főcsoport Jáki kápolnának nevezett épületén.
+A kápolna az eredetitől eltérően egytornyú, amely a kassai Szent Mihály kápolna tornyának motívumaiból is kölcsönzött.
+Az építmény alaprajza viszont az 1207-ben épült lébényi plébániatemploméhoz hasonlít.
+<br><br>
+A kifelé erősen táguló bélletes kapu hat oszloppárja között gazdag ornamentika vezeti a tekintetet az egyenes záródású kapu fölötti félkörívű mezőben elhelyezett domborművű Agnus Dei ábrázoláshoz, és a kapuzatot megkoronázó apostol-galériához.
+A háromszögű oromzat csúcsa alatti, Krisztust ábrázoló szobrot a lépcsőzetes fülkékben elhelyezett 5-5 apostol-szobor veszi körül, egy-egy apostol a kaput közrefogó tornyok homlokzati fülkéjében kapott helyet.
+A kápolnát eredetileg könyvtárként szándékoztak hasznosítani, de a környékbeli hívek kérésére egyházi funkciót kapott.
+Kohl Medárd püspök szentelte fel a kápolnát 1915-ben Szent László király tiszteletére.
+Vallási nevezetessége a kápolnának, hogy 1938-ban szentmisét mutatott itt be Eugenio Pacelli bíboros-nuncius, a későbbi XII. Pius pápa és Giovanni Battista Montini bíboros, a későbbi VI. Pál pápa.
+A kápolnába belépőt a hangsúlyosan elhelyezett feszület (Ohmann Béla műve) fogadja a szembenéző oltár fölött.
+Az oltárkép Mattioni Eszter Szent Családot ábrázoló hímeskő mozaikja, az apszis üvegablakára a tituláris szentet, László királyt festették.
+Az oszlopok a főhajóból két mellékhajóra osztják a teret. Az evangéliumi oldal mellékoltárának triptichonján (Molnár C. Pál) "A jó halált" mutatja be Szent Benedek, Szűz Mária és Morus Szent Tamás boldog halála által.
+Az oltár fölötti üvegablakon Krisztus feltámadásának képe. Az oltáron Strobl Alajos Madonna-szobra áll.
+A leckeoldal hajójában Szent Maximilián Kolbe atyához, az auschwitzi önfeláldozó mártírhoz és a stigmatizált Páter Piohoz (Francesco Forgiole) fohászkodhatnak a hívek.
+A festett üvegablakok Árkay Sztehlo Lili alkotásai, rajtuk az Üdvözítőt, Boldog Ágnest, Boldog Kingát, Szent Erzsébetet, Szent Margitot megjelenítve.
+A keresztút képei modern munkák.
+<br><br>
+Alpár Ignácot 75. születésnapjára (1925) tisztelői bronz-márvány domborműves emléktáblát helyeztek el a Főcsoport gótikus épületszárnyán (Telcs Ede műve).
+A mestert a Kerepesi úti temető árkádsora és a főút közötti díszparcellában temették el.
+Síremlékét Kotál Henrik és Szege Sándor alkotta (1929).
+A Vajdahunyadvár bejárata előtti parkban álló, 24 ezer koronába került emlékművet 1931. november 18-án avatták.
+Az építőművészt középkori céhmesteri öltözékben ábrázolta Telcs Ede. A talapzatot Kotál Henrik tervezte.
